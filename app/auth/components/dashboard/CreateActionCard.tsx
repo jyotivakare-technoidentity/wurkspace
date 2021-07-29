cc1ec05a600079b06b67947da7f005e06340f2fe
Jyotivakare-technoidentity <jyoti.vakare@technoidentity.com>
2021-07-29 17:48:22 +0530

    action items

--- app/auth/components/dashboard/CreateActionCard.tsx
+++ app/auth/components/dashboard/CreateActionCard.tsx
@@ -6,7 +6,13 @@ export const CreateActionCard = () => {
   return (
     <div className="flex items-center justify-center shadow-2xl m-3">
       <div className="rounded-full h-6 w-6 flex items-center justify-center bg-white shadow-2xl">
-        <Link onClick={() => {}}>+</Link>
+        <Link
+          onClick={() => {
+            ;<h1>hello</h1>
+          }}
+        >
+          +
+        </Link>
       </div>
     </div>
   )
