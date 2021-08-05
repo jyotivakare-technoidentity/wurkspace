656ccc10ca40ddcee009211eb3b014913b74c75e
Jyotivakare-technoidentity <jyoti.vakare@technoidentity.com>
2021-08-05 13:59:25 +0530

    modal

--- app/auth/components/dashboard/ActionCard.tsx
+++ app/auth/components/dashboard/ActionCard.tsx
@@ -7,7 +7,7 @@ export const ActionCard = () => {
     <div className="text-sm p-2 ml-2">
       <div className="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
         <a onClick={() => setShowModal(true)}>Check the responsive layout on all devices</a>
-        {showModal ? <Modal data={{ showModal, setShowModal }} /> : ""}
+        {showModal ? <Modal /> : ""}
         <div className="text-grey-darker mt-2 ml-2 flex justify-between items-start">
           <img src="https://i.imgur.com/OZaT7jl.png" className="rounded-full " />
         </div>
