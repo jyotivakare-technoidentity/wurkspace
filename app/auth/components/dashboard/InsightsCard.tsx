8483a37e0bf710a2a404f7e4ba01f0e8ee3f898f
ascharyati <ascharya.prasad@technoidentity.com>
2021-08-10 07:44:22 +0530

    changed master

--- app/auth/components/dashboard/InsightsCard.tsx
+++ app/auth/components/dashboard/InsightsCard.tsx
@@ -1,16 +1,26 @@
+import { Draggable, Droppable } from "react-drag-and-drop"
+
 export const InsightsCard = () => {
   return (
     <>
-      <div className="m-2">
-        <div className="bg-white p-1 rounded shadow-lg relative hover:shadow-2xl">
-          <p className="text-black mb-1 text-xs text-left">
-            Create basic card for youl.Create basic card for youl
-          </p>
-          <button className="m-1 px-1 rounded-full bg-gray-200 text-xs flex left-0">
-            Learning
-          </button>
-        </div>
-      </div>
+      <ul>
+        {" "}
+        <Draggable type="insight">
+          <li>
+            <div className="m-2">
+              <div className="bg-white p-1 rounded shadow-lg relative hover:shadow-2xl">
+                <p className="text-black mb-1 text-xs text-left">
+                  Create basic card for youl.Create basic card for youl
+                </p>
+                <button className="m-1 px-1 rounded-full bg-gray-200 text-xs flex left-0">
+                  Learning
+                </button>
+              </div>
+            </div>
+          </li>
+        </Draggable>
+      </ul>
+
       <div className="m-2">
         <div className="bg-white p-1 rounded shadow-lg relative hover:shadow-2xl">
           <p className="text-black mb-1 text-xs text-left">
